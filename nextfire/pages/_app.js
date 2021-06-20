ef77088b15a8f9672074b9f535815b1689653473
Kevin Sajan <kevsaj@gmail.com>
2021-06-19 23:01:26 -0400

    call Toaster

--- nextfire/pages/_app.js
+++ nextfire/pages/_app.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
