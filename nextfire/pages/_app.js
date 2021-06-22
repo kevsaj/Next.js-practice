9e1c58234e3fa24af7495065882f0fdb7795bded
Kevin Sajan <kevsaj@gmail.com>
2021-06-22 18:00:42 -0400

    use user data in app

--- nextfire/pages/_app.js
+++ nextfire/pages/_app.js
@@ -1,11 +1,14 @@
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
 import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
   
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
